4db150a3e846bb824023aaa3f0c441571d11191d
Justin Arnold <justin.arnold@programmer.net>
2023-08-03 18:40:55 -0400

    feat: render 2 sprites to the canvas
    This commit adds functionality to render two sprites on the canvas. It creates instances of the `Sprite` class for a player and an enemy, and updates their positions using the `update` method. The `animate` function is defined to continuously update and redraw the canvas with each animation frame.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import './tailwind.css'
+import Sprite from './sprite'
 
-
+// Create the canvas and append it to the DOM
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="h-screen w-screen bg-black grid place-items-center overflow-hidden py-8">
         <canvas id="canvas" height="512" width="1024" class="bg-white"></canvas>
     </div>
 `
+
+// Get the canvas and its context
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!
+const ctx = canvas.getContext('2d')!
+
+// define the animate function
+
+const player = new Sprite({ x: 0, y: -150 }, { x: 0, y: 10 })
+const enemy = new Sprite({ x: 100, y: -150 }, { x: 0, y: 10 })
+
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    ctx?.clearRect(0, 0, 1024, 568)
+    player.update(ctx, 512)
+    enemy.update(ctx, 512)
+}
+
+animate()
